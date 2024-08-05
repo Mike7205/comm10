4347815442de40aa55beb3c651a88cae8ca39a67
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-08-05 18:15:30 +0200

    Update comm_t10.py

--- comm_t10.py
+++ comm_t10.py
@@ -74,7 +74,7 @@ def comm_data(comm):
 st.html(
     """
 <style>
-[data-testid="stSidebarContent"] {color: black; background-color: #90EE90} #ADD8E6
+[data-testid="stSidebarContent"] {color: black; background-color: #ADD8E6} #90EE90 
 </style>
 """)
 
