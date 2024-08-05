d3c297d9caacc2e1a38ca672f877de8fa8450519
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-08-05 17:47:54 +0200

    Update comm_t10.py

--- comm_t10.py
+++ comm_t10.py
@@ -34,7 +34,8 @@ comm_dict = {'EURUSD=X':'USD_EUR','CNY=X':'USD/CNY','CL=F':'Crude_Oil',
              '^DJI':'DJI30','GC=F':'Gold','^IXIC':'NASDAQ',
              '^GSPC':'SP_500','^TNX':'10_YB',
              'HG=F':'Copper','GBPUSD=X':'USD_GBP',
-             'JPY=X':'USD_JPY','EURPLN=X':'EUR/PLN','PLN=X':'PLN/USD','CHF=X':'USD/CHF' 
+             'JPY=X':'USD_JPY','EURPLN=X':'EUR/PLN','PLN=X':'PLN/USD',
+             'CHF=X':'USD/CHF','EURCHF=X':'EUR/CHF', 
              ,'^FVX':'5_YB','RUB=X':'USD/RUB','PL=F':'Platinum',
              'SI=F':'Silver','NG=F':'Natural Gas','ZR=F':'Rice Futures',
              'ZS=F':'Soy Futures','KE=F':'KC HRW Wheat Futures'}
