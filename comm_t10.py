c596d72f1678c03aac8bcf613ab1e6a810902809
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-09-27 10:40:55 +0200

    Update comm_t10.py

--- comm_t10.py
+++ comm_t10.py
@@ -21,7 +21,7 @@ from statsmodels.tsa.arima.model import ARIMA
 st.set_page_config(layout="wide")
 
 # start definicji strony
-st.title('Selected global economy indicators & own LSTM prediction models',, divider="blue")
+st.title('Selected global economy indicators & own LSTM prediction models',divider="blue")
 st.write('© Michał Leśniewski \n \n')
 
 # Definicje
