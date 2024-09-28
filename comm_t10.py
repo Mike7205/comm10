0072f8be7a353d227afd86d6e04bb7fe20b9701d
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-09-28 13:09:04 +0200

    Update comm_t10.py

--- comm_t10.py
+++ comm_t10.py
@@ -72,7 +72,7 @@ st.html(
 [data-testid="stSidebarContent"] {color: black; background-color: #CC241C} #90EE90 #ADD8E6 #9CC2CF #91BFCF
 </style>
 """)
-st.subheader('Indexies, Currencies, Bonds & Commodities')
+st.sidebar.subheader('Indexies, Currencies, Bonds & Commodities')
 comm = st.sidebar.radio('',list(comm_dict.values()))
 comm_f(comm)
 st.sidebar.write('© Michał Leśniewski')
