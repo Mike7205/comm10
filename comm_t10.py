e22ad66f019c98914b0e5932c0e770d7afe787c4
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-10-14 10:15:29 +0200

    Update comm_t10.py

--- comm_t10.py
+++ comm_t10.py
@@ -350,7 +350,8 @@ try:
   for item in news:
     st.write(f"Title: {item['title']}")
     #st.write(f"Link: {item['link']}")
-    st.write(f"Link: <a href='{item['link']}' target='_blank'>{item['link']}</a>", unsafe_allow_html=True)
+    #st.write(f"Link: <a href='{item['link']}' target='_blank'>{item['link']}</a>", unsafe_allow_html=True)
+    st.markdown(f"[Link]({item['link']})"
     st.write(f"Publisher: {item['publisher']}\n")     
 except KeyError:
     st.error("KeyError: Symbol not found in Yahoo Finance.")
