864d9070005c0e506c5452043b89273faf9963f4
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-10-15 11:33:05 +0200

    Update comm_t10.py

--- comm_t10.py
+++ comm_t10.py
@@ -317,8 +317,9 @@ def rand_forest(comm, forest):
     future_data = pd.DataFrame({'Date': future_dates,comm: [np.nan] * len(future_dates),
         'Predicted Close': prediction[-forest:]})
     forest_p = pd.concat([forest_p, future_data], ignore_index=True)
-    fig_forest = px.line(forest_p, x='Date', y=[comm, 'Predicted Close'],color_discrete_map={
-                     comm:'green','Predicted Close':'red'}, width=1000, height=500) 
+    forest_p['Predicted Close Mean']= forest_p['Predicted Close'].rolling(window=10).mean()
+    fig_forest = px.line(forest_p, x='Date', y=[comm, 'Predicted Close','Predicted Close Mean'],color_discrete_map={
+                     comm:'green','Predicted Close':'red','Predicted Close Mean':'black'}, width=1000, height=500) 
     fig_forest.update_layout(plot_bgcolor='white',showlegend=True,xaxis=dict(showgrid=True, gridwidth=0.5, gridcolor='Lightgrey'),
                           yaxis=dict(showgrid=True, gridwidth=0.5, gridcolor='Lightgrey'))
     st.plotly_chart(fig_forest, use_container_width=True)
