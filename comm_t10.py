0075ca942c56142719a5876f47f75ff8883c9cfa
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-10-24 10:43:36 +0200

    Update comm_t10.py

--- comm_t10.py
+++ comm_t10.py
@@ -72,7 +72,7 @@ def comm_data(comm):
 st.html(
     """
 <style>
-[data-testid="stSidebarContent"] {color: black; background-color: #CC241C} #90EE90 #ADD8E6 #9CC2CF #91BFCF
+[data-testid="stSidebarContent"] {color: black; background-color: #004481} 
 </style>
 """)
 st.sidebar.subheader('Indexies, Currencies, Bonds, Commodities & Crypto', divider="grey")
