1a3494d974b0446dfbb57ef2d313540fa7a4b656
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-12-10 13:24:11 +0100

    Update comm_t10.py

--- comm_t10.py
+++ comm_t10.py
@@ -32,10 +32,10 @@ today = date.today()
 comm_dict = {'^GSPC':'SP_500','^DJI':'DJI30','^IXIC':'NASDAQ','000001.SS':'SSE Composite Index','^HSI':'HANG SENG INDEX','^VIX':'CBOE Volatility Index','^RUT':'Russell 2000',
              '^BVSP':'IBOVESPA','^FTSE':'FTSE 100','^GDAXI':'DAX PERFORMANCE-INDEX', '^N100':'Euronext 100 Index','^N225':'Nikkei 225','^FCHI':'CAC 40',
              'EURUSD=X':'EUR_USD','EURCHF=X':'EUR_CHF','CNY=X':'USD/CNY', 'GBPUSD=X':'USD_GBP','JPY=X':'USD_JPY','EURPLN=X':'EUR/PLN','PLN=X':'PLN/USD','GBPPLN=X':'PLN/GBP', 
-             'RUB=X':'USD/RUB','EURRUB=X':'EUR/RUB','CNYRUB=X':'CNY/RUB','DX-Y.NYB':'US Dollar Index','^XDE':'Euro Currency Index', '^XDN':'Japanese Yen Currency Index',
+             'RUB=X':'USD/RUB','EURRUB=X':'EUR/RUB','DX-Y.NYB':'US Dollar Index','^XDE':'Euro Currency Index', '^XDN':'Japanese Yen Currency Index',
              '^XDA':'Australian Dollar Currency Index','^XDB':'British Pound Currency Index','^FVX':'5_YB','^TNX':'10_YB','^TYX':'30_YB', 
              'CL=F':'Crude_Oil','BZ=F':'Brent_Oil', 'GC=F':'Gold','HG=F':'Copper', 'PL=F':'Platinum','SI=F':'Silver','NG=F':'Natural Gas',
-              'ZR=F':'Rice Futures','ZS=F':'Soy Futures','BTC-USD':'Bitcoin USD','ETH-USD':'Ethereum USD'}
+              'ZR=F':'Rice Futures','ZS=F':'Soy Futures','BTC-USD':'Bitcoin USD','ETH-USD':'Ethereum USD'} #,'CNYRUB=X':'CNY/RUB'
 
 # Pobieranie danych
 def comm_f(comm):
